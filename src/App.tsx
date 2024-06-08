3a4425f55158a7d048a3d06b63972494ad1e6a49
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-06-08 13:11:12 +0200

    reproduce url issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
+import avatar from "./assets/images/image-avatar.webp";
+
 function App() {
   return (
     <main>
-      <div className="max-w-80 sm:max-w-96 border">
-        test
+      <div className="max-w-80 sm:max-w-96 border flex flex-col">
+        <div></div>
+        <div className="flex flex-col">
+          <div className="">Learning</div>
+          <div className="">Published 21 Dec 2023</div>
+          <h1 className="">HTML & CSS foundations</h1>
+          <p className="">
+            These languages are the backbone of every website, defining
+            structure, content, and presentation.
+          </p>
+        </div>
+        <div className="flex">
+          <img
+            className="max-w-8"
+            src="./assets/images/image-avatar.webp"
+            alt="User's avatar"
+          />
+          <img className="max-w-8" src={avatar} alt="User's avatar" />
+          <h2 className="">Greg Hooper</h2>
+        </div>
       </div>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
