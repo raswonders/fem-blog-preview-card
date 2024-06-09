38fc8d8e528a85df0ab3e5d20b0aa6073016fe14
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-06-09 16:27:22 +0200

    refactor: Update BlogPreviewCard layout

--- src/BlogPreviewCard.tsx
+++ src/BlogPreviewCard.tsx
@@ -4,8 +4,9 @@ import ilustration from "./assets/images/illustration-article.svg";
 export function BlogPreviewCard() {
   return (
     <div className="max-w-80 p-6 sm:max-w-96 flex flex-col items-start gap-6 border border-black rounded-[20px] bg-pureWhite shadow-customSm sm:shadow-customMd">
-      <div className="flex flex-col">
-        <img className="rounded-[10px] bg-yellow" src={ilustration} alt="" />
+      <img className="rounded-[10px] bg-yellow" src={ilustration} alt="" />
+
+      <div className="flex flex-col gap-3">
         <div className="">Learning</div>
         <div className="">Published 21 Dec 2023</div>
         <h1 className="">HTML & CSS foundations</h1>
