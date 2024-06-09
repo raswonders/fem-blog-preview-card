2cee6943da251d6829a5d01591b4da0ce4877999
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-06-09 16:54:36 +0200

    update styles for learning badge

--- src/BlogPreviewCard.tsx
+++ src/BlogPreviewCard.tsx
@@ -3,11 +3,15 @@ import ilustration from "./assets/images/illustration-article.svg";
 
 export function BlogPreviewCard() {
   return (
-    <div className="max-w-80 p-6 sm:max-w-96 flex flex-col items-start gap-6 border border-black rounded-[20px] bg-pureWhite shadow-customSm sm:shadow-customMd">
+    <div className="max-w-80 p-6 sm:max-w-96 flex flex-col items-start gap-6 border border-black rounded-[20px] bg-pureWhite shadow-customSm sm:shadow-customMd text-black">
       <img className="rounded-[10px] bg-yellow" src={ilustration} alt="" />
 
       <div className="flex flex-col gap-3 font-custom">
-        <div className="">Learning</div>
+        <div className="w-fit flex justify-center gap-2 py-1 px-3 rounded bg-yellow">
+          <span className=" text-xs font-extrabold leading-normal">
+            Learning
+          </span>
+        </div>
         <div className="">Published 21 Dec 2023</div>
         <h1 className="">HTML & CSS foundations</h1>
         <p className="">
