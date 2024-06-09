3c7c10869fb012f048b12b79939c632c0e5b27f2
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-06-09 15:31:54 +0200

    chore: Update color variables in index.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: "hsla(0, 0%, 7%, 1)",
+        pureWhite: "hsla(0, 0%, 100%, 1)",
+        yellow: "hsla(47, 89%, 63%, 1)",
+        grey: "hsla(0, 0%, 50%, 1)",
+      },
+    },
   },
   plugins: [],
-}
-
+};
