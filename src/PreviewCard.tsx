d725eb171822325571c532bbcaade243292dc82f
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-06-09 16:09:19 +0200

    chore: Refactor App component to use PreviewCard

---
+++ src/PreviewCard.tsx
@@ -0,0 +1,21 @@
+import avatar from "./assets/images/image-avatar.webp";
+
+export function PreviewCard() {
+  return (
+    <div className="max-w-80 p-6 sm:max-w-96 flex flex-col items-start gap-6 border border-black rounded-[20px] bg-pureWhite shadow-customSm sm:shadow-customMd">
+      <div className="flex flex-col">
+        <div className="">Learning</div>
+        <div className="">Published 21 Dec 2023</div>
+        <h1 className="">HTML & CSS foundations</h1>
+        <p className="">
+          These languages are the backbone of every website, defining structure,
+          content, and presentation.
+        </p>
+      </div>
+      <div className="flex">
+        <img className="max-w-8" src={avatar} alt="User's avatar" />
+        <h2 className="">Greg Hooper</h2>
+      </div>
+    </div>
+  );
+}
