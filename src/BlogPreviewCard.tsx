520ac599d5c631915a45bbada764dfac982d7232
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-06-09 16:21:58 +0200

    feat: Add illustration to BlogPreviewCard

--- src/BlogPreviewCard.tsx
+++ src/BlogPreviewCard.tsx
@@ -1,10 +1,11 @@
 import avatar from "./assets/images/image-avatar.webp";
+import ilustration from "./assets/images/illustration-article.svg";
 
 export function BlogPreviewCard() {
   return (
     <div className="max-w-80 p-6 sm:max-w-96 flex flex-col items-start gap-6 border border-black rounded-[20px] bg-pureWhite shadow-customSm sm:shadow-customMd">
       <div className="flex flex-col">
-        <div className=""></div>
+        <img className="rounded-[10px] bg-yellow" src={ilustration} alt="" />
         <div className="">Learning</div>
         <div className="">Published 21 Dec 2023</div>
         <h1 className="">HTML & CSS foundations</h1>
