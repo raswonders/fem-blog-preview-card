aae5fcb464474d25733bbaee8eec4fcb0359a981
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-06-09 17:02:46 +0200

    style heading

--- src/BlogPreviewCard.tsx
+++ src/BlogPreviewCard.tsx
@@ -8,14 +8,16 @@ export function BlogPreviewCard() {
 
       <div className="flex flex-col gap-3 font-custom">
         <div className="w-fit flex justify-center gap-2 py-1 px-3 rounded bg-yellow">
-          <span className="text-xs font-extrabold leading-normal">
+          <span className="text-xs sm:text-[14px] font-extrabold leading-normal">
             Learning
           </span>
         </div>
-        <div className="text-xs font-medium leading-normal">
+        <div className="text-xs sm:text-[14px] font-normal leading-normal">
           Published 21 Dec 2023
         </div>
-        <h1 className="">HTML & CSS foundations</h1>
+        <h1 className="text-[20px] sm:text-2xl leading-normal font-extrabold hover:text-yellow hover:cursor-pointer">
+          HTML & CSS foundations
+        </h1>
         <p className="">
           These languages are the backbone of every website, defining structure,
           content, and presentation.
